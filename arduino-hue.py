ea26688aed32793dee8999064854ac58f49c9c8b
jpcst <8293342+jpcst@users.noreply.github.com>
2019-04-26 04:13:37 -0300

    Update arduino-hue.py

--- arduino-hue.py
+++ arduino-hue.py
@@ -4,6 +4,7 @@
 import serial
 from phue import Bridge
 import datetime
+
 now = datetime.datetime.now()
 
 with open('C:/Scrape/hue.txt', 'r') as f:
@@ -13,6 +14,7 @@ with open('C:/Scrape/hue.txt', 'r') as f:
 lights = b.get_light_objects('id')
 serial_data = serial.Serial('com3',9600)
 
+i=0
 while True:
 	if(serial_data.inWaiting() > 0):
 		my_data = serial_data.readline().decode().strip()
@@ -22,16 +24,17 @@ while True:
 		teto2 = b.get_light(4,'on') # True = acessa, False = apagada
 		tetos = [teto1,teto2]
 
-		if(tetos[0] == True or tetos[1] == True):
-			b.set_light([2,4], 'on', False, transitiontime=0)
-			print(now.hour,':',now.minute,' -> OFF',sep='')
+		if (i>0):
+			if(tetos[0] == True or tetos[1] == True):
+				b.set_light([2,4], 'on', False, transitiontime=0)
+				print(now.hour,':',now.minute,' -> OFF\n',sep='')
 
+			else:
+				b.set_light([2,4], 'on', True, transitiontime=0)
+				for luz in b.get_light_objects(): # = luzes
+				 	luz.xy=[.3, .3]
+				lights[2].brightness=254
+				lights[4].brightness=254
+				print(now.hour,':',now.minute,' -> ON\n',sep='')
 		else:
-			b.set_light([2,4], 'on', True, transitiontime=0)
-			for luz in b.get_light_objects(): # = luzes
-			 	luz.xy=[.3, .3]
-			lights[2].brightness=254
-			lights[4].brightness=254
-			print(now.hour,':',now.minute,' -> ON',sep='')
-
-		# print(tetos,'\n')
+			i+=1
