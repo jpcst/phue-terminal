e8c5b8e52bcd7bd215ee740bf199f5f8c33f8e75
jpcst <8293342+jpcst@users.noreply.github.com>
2019-04-26 15:06:29 -0300

    Update arduino-hue.py

--- arduino-hue.py
+++ arduino-hue.py
@@ -17,22 +17,21 @@ serial_data = serial.Serial('com3',9600)
 i=0
 while True:
 	if(serial_data.inWaiting() > 0):
-		my_data = serial_data.readline().decode().strip()
-
-		teto1 = b.get_light(2,'on') 
-		teto2 = b.get_light(4,'on') # True = acessa, False = apagada
-		tetos = [teto1,teto2]
-
 		if (i>0):
+			my_data = serial_data.readline().decode().strip()
+			teto1 = b.get_light(2,'on') 
+			teto2 = b.get_light(4,'on') # True = acessa, False = apagada
+			tetos = [teto1,teto2]
 			print(my_data)
+
 			if(tetos[0] == True or tetos[1] == True):
 				b.set_light([2,4], 'on', False, transitiontime=0)
 				print(now.hour,':',now.minute,' -> OFF\n',sep='')
 
 			else:
 				b.set_light([2,4], 'on', True, transitiontime=0)
-				for luz in b.get_light_objects(): # = luzes
-				 	luz.xy=[.3, .3]
+				lights[2].xy = [.3,.3]
+				lights[4].xy = [.3,.3]
 				lights[2].brightness=254
 				lights[4].brightness=254
 				print(now.hour,':',now.minute,' -> ON\n',sep='')
